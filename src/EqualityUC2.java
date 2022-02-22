354e104f72ecef5a7281849d2975afbf2ebde87b
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-02-22 14:44:18 +0530

    Modified file for commenting

--- src/EqualityUC2.java
+++ src/EqualityUC2.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 class Line
 {
+	//Method with return-type for  scanning coordinates of line from user and to solve the line length
 		public static int getLengthOfLine(Scanner scanner) 
 		{
 			System.out.println("Please enter x1");
@@ -15,18 +16,23 @@ class Line
 
 			System.out.println("Please enter y2");
 			int y2 = scanner.nextInt();
-
+			
+			//Calculating Line length
 			int val1 = (int) Math.pow((x2 - x1), 2);
 			int val2 = (int) Math.pow((y2 - y1), 2);
 			int result = (int) Math.sqrt(val1 + val2);
 
 			return result;
 		}
+		//Method with no return type for getting 2lines
 		public void length()
 		{
 			Scanner scanner = new Scanner(System.in);
+			//calling method for line 1 length calculation
 			System.out.println("Please enter values for line 1");
 			int lengthOfline_1 = getLengthOfLine(scanner);
+			
+			//calling method for line 2 length calculation
 			System.out.println("Please enter values for line 2");
 			int lengthOfline_2 = getLengthOfLine(scanner);
 
@@ -42,10 +48,15 @@ class Line
 			scanner.close();
 		}
 }       
-public class EqualityUC2 {
+public class EqualityUC2
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		//creating an object
 		Line equality = new Line();
+		
+		//calling Method
 		equality.length();
 
 	}
